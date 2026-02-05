ff5d57696a3c0f370f9685ff2ed72b7bd5fe2fd4
Daniel <dniel.lopez200610@gmail.com>
2026-02-05 07:56:09 -0500

    Primer commit a mi repositorio

---
+++ ejemplo.py
@@ -0,0 +1,6 @@
+class Estudiante:
+    def __init__(self, nombre, id, telefono):
+        self.nombre = nombre
+        self.id = id
+        self.telefono = telefono
+estudiante1 = Estudiante("Daniel", 1234, 12345678)
